a2dc96a70398e628a81f7eb297b8091546e115ae
Magginetturgaur995812345 <magnusjo294@nemi.tskoli.is>
2018-03-05 23:43:37 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from bottle import *
-import os
+
 import urllib.request, json
 
-with urllib.request.urlopen("http://apis.is/concerts") as url:
+with urllib.request.urlopen("http://apis.is/petrol") as url:
     data = json.loads(url.read().decode())
+
+#print(data)
+
+@route('/Myndir/<filename>')
+def server_static(filename):
+    return static_file(filename, root="./Myndir")
+
 
 @route("/")
 def index():
-    return template("verk5.tpl",data=data)
+    return template("skra.tpl",data=data)
+
+@route("/company/<name>")
+def company(name):
+    return template("company.tpl",data=data,name=name)
 
 run(host="0.0.0.0", port=os.environ.get('PORT'))
-#run(host="localhost", port=8080)
